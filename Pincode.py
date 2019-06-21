ed7ee454f2eb92c3aa7a05ecc66c2fdc65163328
Caesar110094 <45482701+Caesar110094@users.noreply.github.com>
2019-06-21 10:47:35 +0200

    Checking systeem and easteregg
    Checking systeem compressed and easteregg added

--- Pincode.py
+++ Pincode.py
@@ -16,14 +16,39 @@ while quesses < 10:
 
   number = str(number)
   number_two = str(number_two)
-  
-  for index in range(10):
-  if number[index] == number_two[index]: 
-    print("G")  
-    
-  if number[index] != number_two[index]: 
-    print("F") 
 
-    print ("Wrong Code")
-    
+  if len(number_two) != len(number):
+    print("Your input is too long or too short.")
+    quesses = quesses - 1
+    continue
+
+  check = ["F"] * 4
+  digits = [number.count(str(i)) for i in range(10)]
+  count_digits = [i for i in digits]
+
+  if number_two == number and quesses > 1:
+    print("Oof! You've beaten me in " + str(quesses) +" turns!")
+    break
+  else:
+   for i, digit in enumerate(number_two):
+     index = int(digit)
+  if count_digits[index] > 0 and number_two[i] == digit:
+   count_digits[index] -= 1
+   check[i] = 'G'
+  elif count_digits[index] > 0:
+   count_digits[index] -= 1
+   check[i] = 'C'
+
+  
+
+  e1 = "1980"
+
+  if number_two != number and number_two== e1:
+  
+     if number_two == e1:
+        print ("Yeah! You found an easteregg: The birthyear of LGG!")
+     guessesTaken = guessesTaken - 1
+  else:
+     print(*check, sep=" ")
+     print ("Wrong code")
 
