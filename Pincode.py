32e15abf566bf6ce014de1756eb26d93c4d970e3
Caesar110094 <45482701+Caesar110094@users.noreply.github.com>
2019-06-21 09:04:37 +0200

    Update Pincode.py

--- Pincode.py
+++ Pincode.py
@@ -2,42 +2,33 @@ import random
 pincode = ["1221", "9997", "8829", "6765", "9114", "5673", "0103", "4370", "8301", "1022"]
 number = (random.choice (pincode))
 print(number)
-quesses = str(number)
-easter = ["1980"]
+
 
 number_two = (input("Guess the random 4 digit number: "))
 
 quesses = 0
-while quesses < 5:
-    quesses +=1
-    print ("this is your %s guess" %(number_two))
-    if number_two < number:
-        print ("guess is low")
-        break
-    elif number_two > number:
-        print ("guess is high") 
-        break
-    elif number_two == number:
-        guesses = str(guesses)
-        print ("You guess it in : ", guesses + " guesses")
-        break
-    elif number_two == easter:
-        print ("Yeah!")
-    
+while quesses < 10:
+    quesses +=1    
+    print ("This is your guess: %s" %(number_two))
+    if number_two == number:
+        quesses = str(quesses)
+        print ("You guess it in : ", quesses + " guesses")
+    break
+
 number = str(number)
 number_two = str(number_two)
 if number == number_two:
-  print("correct")
-else:  
+  print("You guessed it!")
+else:
   if number[0] == number_two[0]: 
-    print("G") 
+    print("G")  
   if number[1] == number_two[1]: 
     print("G") 
   if number[2] == number_two[2]: 
     print("G")
   if number[3] == number_two[3]: 
-    print("G")  
-
+    print("G")
+  
   if number[0] != number_two[0]: 
     print("F") 
   if number[1] != number_two[1]: 
@@ -45,4 +36,8 @@ else:
   if number[2] != number_two[2]: 
     print("F") 
   if number[3] != number_two[3]: 
-    print("F")  
+    print("F") 
+  if number != number_two:
+    print ("Wrong Code")
+    
+
