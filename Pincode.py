92008281b68407a31b41b959681d297cf795a5ee
Caesar110094 <45482701+Caesar110094@users.noreply.github.com>
2019-06-28 09:30:17 +0200

    Update Pincode.py

--- Pincode.py
+++ Pincode.py
@@ -5,24 +5,24 @@ print(number)
 
 quesses = 0
 while quesses < 10:
-  number_two = input("Guess the random 4 digit number: ")
+  userinput = input("Guess the random 4 digit number: ")
 
   quesses += 1    
-  print ("This is your guess: %s" %(number_two))
+  print ("This is your guess: %s" %(userinput))
   print ("You have used " + str(quesses) + " out of 10 guesses")
-  if number_two == number:
+  if userinput == number:
     quesses2 = str(quesses)
     print ("You guess it in:", quesses2 + " guesses")  
 
   number = str(number)
-  number_two = str(number_two)
+  userinput = str(userinput)
   
-  if number_two.isdigit() == False:
+  if userinput.isdigit() == False:
     print ("Error: You can only use numbers")
     quesses = quesses - 1
     continue
   
-  if len(number_two) != len(number):
+  if len(userinput) != len(number):
     print("Your input is too long or too short.")
     quesses = quesses - 1
     continue
@@ -30,21 +30,23 @@ while quesses < 10:
 
 
   check = ["F"] * 4
-  digits = [number.count(str(i)) for i in range(10)]
-  count_digits = [i for i in digits]
-
-  if number_two == number and quesses > 1:
+  if userinput == number and quesses > 1:
     print("The game was beaten in " + str(quesses) +" quesses. Congratulations!")
     break
   else:
-   for i, digit in enumerate(number_two):
-     index = int(digit)
-  if count_digits[index] > 0 and number_two[i] == digit:
-   count_digits[index] == 1
-   check[i] = 'G'
-  if count_digits[index] > 0:
-   count_digits[index] -= 1
-   check[i] = 'C'
+    for idx, digit in enumerate(userinput):     
+      #als het nummer op de goede plek staat, print G
+      if number[idx] == digit:       
+        check[idx] = "G"
+      
+      #als het nummer vookomt, print C
+      elif digit in number:
+        check[idx] = "C"
+      
+      #anders, print F
+      else:        
+        check[idx] = "F"
+      
 
   e1 = "1980"
   e2 = "1955"
